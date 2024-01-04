3ca2313093873e77ca3e632071daec0e0cd57661
Chen Pyng Haw <pynghaw5@gmail.com>
2024-01-04 16:14:10 +0800

    y

--- const_matlist.php
+++ const_matlist.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Quixlab - Bootstrap Admin Dashboard Template by Themefisher.com</title>
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-    <!-- Custom Stylesheet -->
-    <link href="./plugins/tables/css/datatable/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-</head>
-
+<?php include 'headermain.php'; ?>
 <body>
 
     <!--*******************
@@ -936,5 +921,4 @@
     <script src="./plugins/tables/js/datatable-init/datatable-basic.min.js"></script>
 
 </body>
-
-</html>
+<?php include 'footer.php'; ?>
