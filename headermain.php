f4e791240f373572a9fc4d0c49001000cf5f5ae3
Chen Pyng Haw <pynghaw5@gmail.com>
2024-01-04 21:11:55 +0800

    update

--- headermain.php
+++ headermain.php
@@ -259,7 +259,7 @@
                             <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
                         </a>
                         <ul aria-expanded="false">
-                            <li><a href="./index.html">Home 1</a></li>
+                            <li><a href="./index.php">Home 1</a></li>
                             <!-- <li><a href="./index-2.html">Home 2</a></li> -->
                         </ul>
                     </li>
