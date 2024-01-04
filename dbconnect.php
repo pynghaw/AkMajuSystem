821cabf82bda632dc3879b44b916185d01693584
tiewrong <129280403+tiewrong@users.noreply.github.com>
2024-01-04 15:43:24 +0800

    Create dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,16 @@
+<?php 
+
+//Parameters
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="db_cryptoknights";
+
+//Connection
+$con=mysqli_connect($servername, $username, $password, $dbname);
+
+
+//Verify connection
+
+
+?>
