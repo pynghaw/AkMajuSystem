c7892810b90a6e0624858ed0d8075b4f616de481
tiewrong <129280403+tiewrong@users.noreply.github.com>
2024-01-05 20:53:11 +0800

    commit

--- forgot password.php
+++ forgot password.php
@@ -50,7 +50,7 @@
                                         <p class="mb-4">We get it, stuff happens. Just enter your email address below
                                             and we'll send you a link to reset your password!</p>
                                     </div>
-                                    <form >
+                                    <form action="forgot_password.php" method="post">
     <div class="form-group">
         <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
     </div>
