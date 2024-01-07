f7f72ec2448426a18f29788b2076c3104470aa0f
tiewrong <129280403+tiewrong@users.noreply.github.com>
2024-01-08 02:25:48 +0800

    update

--- resetPasswordProcess.php
+++ resetPasswordProcess.php
@@ -1,26 +1,55 @@
 <?php
 
-// Connect to DB
-include("dbconnect.php");
 
-// Retrieve data from registration form
+if (isset($_GET['code'])) {
+    $code = $_GET['code'];
+
+    if($conn->connect_error){
+        die('Could not connect to the databse');
+    }
+
+// Connect to DB
+$conn = new mySqli('localhost', 'root', '', 'db_cryptoknights');
+
 
 $fpwd = password_hash($_POST['fpwd'], PASSWORD_DEFAULT); // Hash the password
 
+$verifyQuery = $conn->query("SELECT * FROM tb_user WHERE code='$code' AND updated_time >= NOW() - Interval 1 DAY");
 
-// CRUD Operation
-// Create - SQL Insert Statement
-$sql = "UPDATE tb_user
-        SET u_pwd='$fpwd'";
+
+
+if(isset($_POST['email'])){
+        $email= $_POST['email'];
+        $new_password = $_POST['fpwd'];
+        $new_password = password_hash($_POST['fpwd'], PASSWORD_DEFAULT); // Hash the password
+
+
+        $changeQuery = $conn->query("UPDATE tb_user SET u_pwd='$new_password' WHERE code='$code' AND updated_time >= NOW() - Interval 1 DAY");
+
+
+
+
+if($changeQuery){
+        header("Location: passwordchange.php");
+}
+
+$conn-close();
+}else{
+       header("Location: resetPassword.php"); 
+}
+
       
-
-// Execute SQL
-mysqli_query($con, $sql);
-
-// Close DB Connection
-mysqli_close($con);
 
 // Redirect to the next page
 header("Location:index.php");
+
+} else {
+    // Handle the case when $code is not set
+    die("Error: Code is not set. Make sure you have the correct reset link.");
+}
+
+
+
+
 
 ?>
