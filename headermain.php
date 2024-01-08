65eff35d73ff4e9dd675b718c54b91e7c56829e9
tiewrong <129280403+tiewrong@users.noreply.github.com>
2024-01-08 22:57:56 +0800

    update

--- headermain.php
+++ headermain.php
@@ -110,6 +110,15 @@
         <div class="nk-sidebar">           
             <div class="nk-nav-scroll">
                 <ul class="metismenu" id="menu">
+                    <li class="nav-label">Dashboard</li>
+                    <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="staff.php">Home 1</a></li>
+                            <!-- <li><a href="./index-2.html">Home 2</a></li> -->
+                        </ul>
                     
                     <li class="nav-label">Apps</li>
                       <li>
