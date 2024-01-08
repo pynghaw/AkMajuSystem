eeed407cf0fc0cb94c0392b4b951a60641175bbd
tiewrong <129280403+tiewrong@users.noreply.github.com>
2024-01-08 22:34:32 +0800

    Update headermain.php

--- headermain.php
+++ headermain.php
@@ -71,17 +71,7 @@
                     </div>
                 </div>
                 <div class="header-left">
-                    <div class="input-group icons">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text bg-transparent border-0 pr-2 pr-sm-3" id="basic-addon1"><i class="mdi mdi-magnify"></i></span>
-                        </div>
-                        <input type="search" class="form-control" placeholder="Search Dashboard" aria-label="Search Dashboard">
-                        <div class="drop-down animated flipInX d-md-none">
-                            <form action="#">
-                                <input type="text" class="form-control" placeholder="Search">
-                            </form>
-                        </div>
-                    </div>
+                   
                 </div>
                 <div class="header-right">
                     <ul>
