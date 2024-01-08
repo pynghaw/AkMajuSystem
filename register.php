704111f13b19af84e81caa4f239538061cab678e
tiewrong <129280403+tiewrong@users.noreply.github.com>
2024-01-08 14:54:47 +0800

    update

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>AK MAJU</title>
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
+     <link rel="icon" type="image/jpg" sizes="16x16" href="images/akmaju.jpg">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
     
