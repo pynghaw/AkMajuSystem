447d91bcc7ecf96fd7206f57e6f8c9e5ea42d602
tiewrong <129280403+tiewrong@users.noreply.github.com>
2024-01-08 22:48:04 +0800

    Update headermain.php

--- headermain.php
+++ headermain.php
@@ -110,36 +110,13 @@
         <div class="nk-sidebar">           
             <div class="nk-nav-scroll">
                 <ul class="metismenu" id="menu">
-                    <li class="nav-label">Dashboard</li>
-                    <li>
-                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
-                            <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="./index.php">Home 1</a></li>
-                            <!-- <li><a href="./index-2.html">Home 2</a></li> -->
-                        </ul>
-                    </li>
-                    <li class="mega-menu mega-menu-sm">
-                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
-                            <i class="icon-globe-alt menu-icon"></i><span class="nav-text">Layouts</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="./layout-blank.html">Blank</a></li>
-                            <li><a href="./layout-one-column.html">One Column</a></li>
-                            <li><a href="./layout-two-column.html">Two column</a></li>
-                            <li><a href="./layout-compact-nav.html">Compact Nav</a></li>
-                            <li><a href="./layout-vertical.html">Vertical</a></li>
-                            <li><a href="./layout-horizontal.html">Horizontal</a></li>
-                            <li><a href="./layout-boxed.html">Boxed</a></li>
-                            <li><a href="./layout-wide.html">Wide</a></li>
-                            
-                            
-                            <li><a href="./layout-fixed-header.html">Fixed Header</a></li>
-                            <li><a href="layout-fixed-sidebar.html">Fixed Sidebar</a></li>
-                        </ul>
-                    </li>
+                    
                     <li class="nav-label">Apps</li>
+                      <li>
+                        <a href="profile.php" aria-expanded="false">
+                            <i class="icon-user "></i><span class="nav-text">Profile</span>
+                        </a>
+                    </li>
                     <li>
                         <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                            <i class="fa fa-archive"></i> <span class="nav-text">Inventory</span>
@@ -150,6 +127,17 @@
                             
                         </ul>
                     </li>
+                     <li>
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="icon-envelope menu-icon"></i> <span class="nav-text">Customer</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="customer-add.php">Add Customer</a></li>
+                            <li><a href="customer-manage.php">Manage Customer</a></li>
+                            
+                        </ul>
+                    </li>
+                  
                     <li>
                         <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                             <i class="icon-envelope menu-icon"></i> <span class="nav-text">Order</span>
@@ -205,21 +193,7 @@
                             
                         </ul>
                     </li>
-                    <li>
-                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
-                            <i class="icon-envelope menu-icon"></i> <span class="nav-text">Customer</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="customer-add.php">Add Customer</a></li>
-                            <li><a href="customer-manage.php">Manage Customer</a></li>
-                            
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="profile.php" aria-expanded="false">
-                            <i class="icon-badge menu-icon"></i><span class="nav-text">Profile</span>
-                        </a>
-                    </li>
+                   
 
                     <!-- Other components -->
                     <li class="nav-label">UI Components</li>
