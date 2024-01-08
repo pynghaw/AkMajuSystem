0d87048a1ba2c8e94d1fd1ac90d8fdeca282b4dd
tiewrong <129280403+tiewrong@users.noreply.github.com>
2024-01-09 02:12:33 +0800

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -22,7 +22,7 @@ try {
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'tiewchuanrong@gmail.com';                     //SMTP username
     $mail->Password   = 'myql jgou kxew xmaa';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
