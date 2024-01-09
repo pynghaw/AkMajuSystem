ce9c40e2a4757391da802a4c52cc80cf0805e59e
Wong Jun Ji <129287788+jjwong2003@users.noreply.github.com>
2024-01-09 11:49:21 +0800

    Update order-place.php

--- order-place.php
+++ order-place.php
@@ -28,7 +28,18 @@ include 'headermain.php'; ?>
 
 <body>
 
-       <div class="content-body">
+<div class="content-body">
+
+    <div class="row page-titles mx-0">
+        <div class="col p-md-0">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
+                <li class="breadcrumb-item active"><a href="javascript:void(0)">Place Orders</a></li>
+                
+            </ol>
+        </div>
+    </div>
+
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
