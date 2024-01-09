3751f95c4e677d6d1a76ef260c2888ae9e4d890c
Wong Jun Ji <129287788+jjwong2003@users.noreply.github.com>
2024-01-09 11:43:44 +0800

    meow

---
+++ invoice-delete-success.php
@@ -0,0 +1,26 @@
+<?php include 'headermain.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoice Deleted Successfully</title>
+</head>
+<body>
+    <div class="content-body">
+        <div class="container-fluid">
+            <!-- Your page content goes here -->
+
+            <script>
+                // JavaScript code to show a prompt
+                alert("Quotation Deleted Successfully");
+                // Redirect to another page
+                window.location.href = "invoice-manage.php"; // Change "index.php" to the desired page
+            </script>
+        </div>
+    </div>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
