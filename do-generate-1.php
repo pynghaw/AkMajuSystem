bed60cd64a9d4b8649af77aab484a9d2e4397a18
Wong Jun Ji <129287788+jjwong2003@users.noreply.github.com>
2024-01-15 11:14:08 +0800

    do patch

--- do-generate-1.php
+++ do-generate-1.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="col p-md-0">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
-                <li class="breadcrumb-item active"><a href="javascript:void(0)">Home</a></li>
+                <li class="breadcrumb-item active"><a href="javascript:void(0)">Generate DO</a></li>
             </ol>
         </div>
     </div>
